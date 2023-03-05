c94c5b534e27a724746c3ac58f6f25e5e9c7f3a4
SangLT <lethanhsang14082001@gmail.com>
2023-03-05 20:49:31 +0700

    Add Steps and Locks

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -8,12 +8,20 @@ public class Piece : MonoBehaviour
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; }
 
+    public float stepDelay = 1f;
+    public float lockDelay = 0.5f;
+
+    private float stepTime;
+    private float lockTime;
+
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
         this.board = board;
         this.position = position;
         this.data = data;
         this.rotationIndex= 0;
+        this.stepTime = Time.time + this.stepDelay;
+        this.lockTime = 0f;
 
         if(this.cells == null)
         {
@@ -31,6 +39,8 @@ public class Piece : MonoBehaviour
     private void Update()
     {
         this.board.Clear(this);
+
+        this.lockTime += Time.deltaTime;
 
         // Rotation
         if (Input.GetKeyDown(KeyCode.Q)) // Left Rotation
@@ -59,7 +69,30 @@ public class Piece : MonoBehaviour
             HardDrop();
         }
 
+        if(Time.time >= this.stepTime)
+        {
+            Step();
+        }
+
         this.board.Set(this);
+    }
+
+    private void Step()
+    {
+        this.stepTime = Time.time + this.stepDelay;
+        
+        Move(Vector2Int.down);
+
+        if (this.lockTime >= this.lockDelay)
+        {
+            Lock();
+        }
+    }
+
+    private void Lock()
+    {
+        this.board.Set(this);
+        this.board.SpawnPiece();
     }
 
     private void HardDrop()
@@ -68,6 +101,7 @@ public class Piece : MonoBehaviour
         {
             continue;
         }
+        Lock();
     }
 
     private bool Move(Vector2Int translation)
@@ -81,6 +115,7 @@ public class Piece : MonoBehaviour
         if (valid)
         {
             this.position = newPosition;
+            this.lockTime = 0f;
         }
 
         return valid;
@@ -89,7 +124,7 @@ public class Piece : MonoBehaviour
     private void Rotate(int direction)
     {
         int originalRotation = this.rotationIndex;
-        this.rotationIndex += Wrap(this.rotationIndex + direction, 0, 4);
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
 
         ApplyRotationMatrix(direction);
 
